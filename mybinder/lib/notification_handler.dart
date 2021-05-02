9789abb84b97932624c05b8d698a98ec8b3f49b4
Vinicius Ibanez <vinicius.ibanez97@gmail.com>
2021-05-02 13:49:23 -0300

    add push notification handler

---
+++ mybinder/lib/notification_handler.dart
@@ -0,0 +1,27 @@
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationHandler {
+
+  NotificationHandler._();
+
+  factory NotificationHandler() => _instance;
+
+  static final NotificationHandler _instance = NotificationHandler._();
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  bool _initialized = false;
+
+  Future<void> init() async {
+    if (!_initialized) {
+ 
+      _firebaseMessaging.requestNotificationPermissions();
+      _firebaseMessaging.configure();
+
+      String token = await _firebaseMessaging.getToken();
+      print("FirebaseMessaging token: $token");
+      
+      _initialized = true;
+    }
+  }
+}
