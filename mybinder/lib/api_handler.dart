7e60622bbe3695f67b9b56a2842bfd68dce6dcb1
ibanez <vinicius.ibanez97@gmail.com>
2021-05-25 10:51:55 -0300

    update api handler

--- mybinder/lib/api_handler.dart
+++ mybinder/lib/api_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:convert' as convert;
@@ -5,7 +7,6 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class ApiHandler {
-
   ApiHandler._();
 
   factory ApiHandler() => _instance;
@@ -15,26 +16,22 @@ class ApiHandler {
   bool _initialized = false;
   String _token = "";
 
-  Future<void> init() async {
-    
+  Future<void> init() async {}
+
+  Future<void> insertUser() async {
+    // This example uses the Google Books API to search for books about http.
+    // https://developers.google.com/books/docs/overview
+    //var url = Uri.https('my-binder-api-staging.herokuapp.com', '/user/insert');
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = http.post(
+      Uri.parse('https://my-binder-api-staging.herokuapp.com/user/insert'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'user': 'ibanez',
+      }),
+    );
   }
-
-  void insertUser() async {
-  // This example uses the Google Books API to search for books about http.
-  // https://developers.google.com/books/docs/overview
-  var url =
-      Uri.https('my-binder-api-staging.herokuapp.com', '/user/insert', {'user': 'teste'});
-
-  // Await the http get response, then decode the json-formatted response.
-  var response = await http.get(url);
-  if (response.statusCode == 200) {
-    // var jsonResponse =
-    //     convert.jsonDecode(response.body) as Map<String, dynamic>;
-    // var itemCount = jsonResponse['totalItems'];
-    print('Deu certo');
-  } else {
-    print('Request failed with status: ${response.statusCode}.');
-  }
-}
-
 }
